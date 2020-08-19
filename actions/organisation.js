13477432dbd62c34658aef69615bd8210213865e
Rich Wilson <richard.wilson@smgdesign.org>
2020-08-20 00:59:02 +0100

    added log for values

--- actions/organisation.js
+++ actions/organisation.js
@@ -32,7 +32,7 @@ const create = async ({domain, action, command, socketId, payload, user}) => {
       index.push(`$${fields.length}`);
     }
     const queryString = `INSERT INTO organisations (${fields.join(',')}) VALUES (${index.join(',')}) RETURNING public_id`;
-    console.log('query', queryString);
+    console.log('query', queryString, values);
     const organisation = await pgPool.query(queryString, values);
     publish('ex-gateway', { domain, action, command, payload: { ...payload, public_id: organisation.rows[0].public_id }, user, socketId });
   } catch (error) {
